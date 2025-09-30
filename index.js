8817dd79c48b323a6216882ded04151edf9fa5e3
rishamsandhuu <rishamsandhuu@gmail.com>
2025-09-30 18:49:29 -0400

    Ignoring .env file

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Example route
+app.get('/', (req, res) => {
+    res.send('Hello MERN Backend!');
+});
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
