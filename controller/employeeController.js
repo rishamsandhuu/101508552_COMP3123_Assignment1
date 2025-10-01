30c5cba7e9e7949b5954221c28959fc85c563a9d
rishamsandhuu <rishamsandhuu@gmail.com>
2025-09-30 22:57:04 -0400

    User can create employee

---
+++ controller/employeeController.js
@@ -0,0 +1,15 @@
+const Employee = require('../model/employee');
+
+// Create a new employee
+exports.createEmployee = async (req, res) => {
+  try {
+    const employee = new Employee(req.body);
+    await employee.save();
+    res.status(201).json({ message: 'Employee created successfully', "employee_id": employee._id  });
+  } catch (err) {
+    if (err.code === 11000) {
+      return res.status(400).json({ error: 'Email already exists' });
+    }
+    res.status(400).json({ error: err.message });
+  }
+};
--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const cors = require('cors');
 require('dotenv').config();
 
 const userRoutes = require('./routes/userRoutes');
+const employeeRoutes = require('./routes/employeeRoutes');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Use modular routes
 app.use('/user', userRoutes);
+app.use('/emp', employeeRoutes);
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
