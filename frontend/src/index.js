48542a0882bf3dcd48311d5d8626aa3cba9ca3f4
rishamsandhuu <rishamsandhuu@gmail.com>
2025-11-30 22:09:25 -0500

    fixing import statement

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles/App.css';
+import './App.css';
 import './index.css'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
