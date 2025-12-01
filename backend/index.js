d4135b94c3c178672e7b260ec8d368e1eb978d9c
rishamsandhuu <rishamsandhuu@gmail.com>
2025-11-30 19:16:57 -0500

    vercel configuration

--- backend/index.js
+++ backend/index.js
@@ -24,4 +24,4 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .catch(err => console.log(err));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
